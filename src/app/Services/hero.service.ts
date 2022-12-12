50abe4828b9dfdebe3261ff88a2cc1fe0bb628f2
HBouabdallah <haythem.bouabdallah@gmail.com>
2022-12-12 11:28:12 +0100

    fix json module

--- src/app/Services/hero.service.ts
+++ src/app/Services/hero.service.ts
@@ -2,13 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { filter, Observable } from 'rxjs';
 import { IHero } from '../Models/IHeros';
-
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  apiUrl = 'assets/data/BDD_Hero.json'
+  apiUrl = '../../assets/data/BDD_Hero.json'
   constructor(private http: HttpClient) { }
   GetHeros(): Observable <IHero[]>{
     let heroList = this.http.get<IHero[]>(this.apiUrl);
